aee6a6358453a24551521230abc7d57f88b43e52
Gopeshworyadav <np01nt4s230018@islingtoncollege.edu.np>
2026-01-16 12:18:45 +0545

    Create firewall.py

---
+++ firewall.py
@@ -0,0 +1,10 @@
+# firewall.py
+from datetime import datetime
+
+def drop_packet_info(packet_info):
+    print(
+        f"[IPS DROP] {datetime.now()} | "
+        f"{packet_info['src_addr']}:{packet_info['src_port']} → "
+        f"{packet_info['dst_addr']}:{packet_info['dst_port']} | "
+        f"Attack: {packet_info['alert']}"
+    )
