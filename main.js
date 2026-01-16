3acc88bb218fbfe964208946e816e5360d3f74d0
Gopeshworyadav <np01nt4s230018@islingtoncollege.edu.np>
2026-01-16 12:23:07 +0545

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+async function start(){
+  await fetch('/start', {method:'POST'});
+  setTimeout(fetchAlerts, 500);
+}
+async function stop(){
+  await fetch('/stop', {method:'POST'});
+  setTimeout(fetchAlerts, 500);
+}
+async function fetchAlerts(){
+  const res = await fetch('/alerts');
+  const data = await res.json();
+  const tbody = document.querySelector('#alerts tbody');
+  tbody.innerHTML = '';
+  for(const a of data.reverse()){
+    const tr = document.createElement('tr');
+    const now = new Date().toLocaleString();
+    tr.innerHTML = `<td>${now}</td><td>${a.src}:${a.src_port||''}</td><td>${a.dst}:${a.dst_port||''}</td><td>${a.alert}</td><td><pre style="white-space:pre-wrap">${a.payload_preview||''}</pre></td>`;
+    tbody.appendChild(tr);
+  }
+}
